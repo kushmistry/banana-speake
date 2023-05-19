a03445c435e1007f6ba06a8af46375a6404c4810
Kush Mistry <kushmistry16@gmail.com>
2023-05-19 14:53:40 +0530

    Added content in app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+let inputText = document.querySelector("#input-txt");
+let tranlateBtn = document.querySelector("#translate-btn");
+let outputText = document.querySelector(".output-txt");
 
+const serverUrl = "https://api.funtranslations.com/translate/minion.json";
+const mockSereverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" 
+
+const createUrl = (text) => {
+  return `${mockSereverUrl}?text=${text}`;
+};
+
+const btnHandler = () => {
+  fetch(createUrl(inputText.value))
+    .then(response => response.json())
+    .then(json => {
+        outputText.innerText = json.contents.translated;
+    })
+};
+
+tranlateBtn.addEventListener("click", btnHandler);
